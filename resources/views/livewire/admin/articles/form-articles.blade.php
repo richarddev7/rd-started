4ecfdb73a40b11b77fa32c0ecc1aebae8262f49e
Richard Dev <richarddev7@gmail.com>
2024-04-06 00:32:36 -0500

    .- validate field content (Page & Articles)

--- resources/views/livewire/admin/articles/form-articles.blade.php
+++ resources/views/livewire/admin/articles/form-articles.blade.php
@@ -28,6 +28,11 @@
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">ERROR!</span> {{ $message }}</p>
                     @enderror
                 </div>
+                <div class="sm:col-span-3 mb-4">
+                    @error('formFieldContent')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">ERROR!</span> {{ $message }}</p>
+                    @enderror
+                </div>
                 <div class="sm:col-span-3 mb-4 border-t-4 border-indigo-500 pt-4">
                     <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">@lang('Title seo')</label>
                     <input type="text" wire:model="seoTitle" id="seoTitle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
--- resources/views/livewire/admin/pages/form-pages.blade.php
+++ resources/views/livewire/admin/pages/form-pages.blade.php
@@ -24,6 +24,8 @@
                 <div class="sm:col-span-3 mb-4" wire:ignore>
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">@lang('Content')</label>
                     <textarea wire:model="formFieldContent" id="formFieldContent" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="@lang('Contetnt')"></textarea>
+                </div>
+                <div class="sm:col-span-3 mb-4">
                     @error('formFieldContent')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">ERROR!</span> {{ $message }}</p>
                     @enderror
